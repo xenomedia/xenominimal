1c862c5c1a4eb8e2041ed15155eb9e88c86dd7d3
Jim Birch <jim@xenomedia.com>
2017-08-11 11:57:46 -0500

    Init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Xenominimal
+
+Drupal 8 theme. Child of the Adminimal Admin theme.
+
+**Installation:**
+
+  * Install the theme as you normally would.
+
+**Requirements:**
+
+  * [Adminimal Theme](https://www.drupal.org/project/adminimal_theme)
---
+++ js/xenominimal.js
@@ -0,0 +1,21 @@
+/*
+================================================
+Custom Javascript scripts
+================================================
+*/
+
+/* check if browser is IE10 */
+if (navigator.userAgent.match('MSIE 10.0;')) {
+  $('html').addClass('lt-ie11 ie10');
+}
+
+/*
+================================================
+Custom jQuery scripts
+================================================
+*/
+
+jQuery(document).ready(function($) {
+  'use strict';
+
+});
